7143cc99064ed43166d9ad7710599956d074c967
Aditya <116873744+PanditAdityaJi@users.noreply.github.com>
2025-07-18 21:28:44 +0530

    cdc_fifo.v

--- src/cdc_fifo.v
+++ src/cdc_fifo.v
@@ -1,28 +1,16 @@
-module tb_cdc_fifo;
-  logic wr_clk, rd_clk, wr_en, rd_en;
-  logic [7:0] data_in;
-  logic [7:0] data_out;
-  logic empty, full;
+logic [ADDR_WIDTH-1:0] wr_ptr_gray, rd_ptr_gray;
+logic [ADDR_WIDTH-1:0] wr_ptr_gray_sync, rd_ptr_gray_sync;
 
-  cdc_fifo #(.WIDTH(8), .DEPTH(16)) u0 (
-    .wr_clk(wr_clk), .rd_clk(rd_clk),
-    .wr_en(wr_en), .rd_en(rd_en),
-    .data_in(data_in), .data_out(data_out),
-    .empty(empty), .full(full)
-  );
-
-  initial begin
-    wr_clk = 0; rd_clk = 0;
-    forever begin
-      #5 wr_clk = ~wr_clk;
-      #7 rd_clk = ~rd_clk;
-    end
-  end
-
-  initial begin
-    wr_en = 0; rd_en = 0; data_in = 8'hA5;
-    #15 wr_en = 1;
-    #30 wr_en = 0; rd_en = 1;
-    #50 $finish;
-  end
+module cdc_fifo #(parameter WIDTH=8, DEPTH=16)(
+    input  logic wr_clk,
+    input  logic rd_clk,
+    input  logic [WIDTH-1:0] data_in,
+    input  logic wr_en,
+    input  logic rd_en,
+    output logic [WIDTH-1:0] data_out,
+    output logic empty,
+    output logic full
+);
+  always_ff @(posedge wr_clk) begin
+    {wr_ptr_gray_sync[1], wr_ptr_gray_sync[0]} <= {wr_ptr_gray_sync[0], rd_ptr_gray};
 endmodule
