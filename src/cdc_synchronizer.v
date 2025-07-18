8407b38acde30194aa8ae8ab987becab02ca5e1b
Aditya <116873744+PanditAdityaJi@users.noreply.github.com>
2025-07-18 21:26:12 +0530

    cdc_synchronizer.v

--- src/cdc_synchronizer.v
+++ src/cdc_synchronizer.v
@@ -1,16 +1,12 @@
-module tb_cdc_synchronizer;
-  logic clk, async_sig, sync_out;
-  cdc_synchronizer u0 (.clk(clk), .async_sig(async_sig), .sync_out(sync_out));
-
-  initial begin
-    clk = 0;
-    forever #10 clk = ~clk;
-  end
-
-  initial begin
-    async_sig = 0;
-    #25 async_sig = 1;
-    #35 async_sig = 0;
-    #40 $finish;
-  end
+module cdc_synchronizer(
+    input  logic async_sig,
+    input  logic clk,
+    output logic sync_out
+);
+    logic sync_ff1, sync_ff2;
+    always_ff @(posedge clk) begin
+        sync_ff1 <= async_sig;
+        sync_ff2 <= sync_ff1;
+    end
+    assign sync_out = sync_ff2;
 endmodule
