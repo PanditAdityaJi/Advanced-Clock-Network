042268bcba4d605f879bcea502eb51bb4984e125
Aditya <116873744+PanditAdityaJi@users.noreply.github.com>
2025-07-18 21:23:54 +0530

    phase_interpolator.v

--- src/phase_interpolator.v
+++ src/phase_interpolator.v
@@ -1,19 +1,7 @@
-module tb_phase_interpolator;
-  logic [5:0] phase_sel;
-  logic base_clk;
-  logic interp_clk;
-
-  phase_interpolator u0 (.phase_sel(phase_sel), .base_clk(base_clk), .interp_clk(interp_clk));
-
-  initial begin
-    base_clk = 0;
-    forever #10 base_clk = ~base_clk;
-  end
-
-  initial begin
-    phase_sel = 6'b000000;
-    #30 phase_sel = 6'b111111;
-    #40 phase_sel = 6'b101010;
-    #50 $finish;
-  end
+module phase_interpolator(
+    input  logic [5:0] phase_sel,
+    input  logic base_clk,
+    output logic interp_clk
+);
+    assign interp_clk = base_clk ^ phase_sel[0];
 endmodule
