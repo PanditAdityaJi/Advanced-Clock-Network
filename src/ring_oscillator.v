0c7f452eea8076c3c8dea1ac66c198ba3b665324
Aditya <116873744+PanditAdityaJi@users.noreply.github.com>
2025-07-18 21:22:45 +0530

    ring_oscillator.v

--- src/ring_oscillator.v
+++ src/ring_oscillator.v
@@ -1,11 +1,16 @@
-module tb_ring_oscillator;
-  logic enable;
-  logic clk_out;
-  ring_oscillator #(.STAGES(5)) u0 (.enable(enable), .clk_out(clk_out));
-
-  initial begin
-    enable = 0;
-    #10 enable = 1;
-    #100 $finish;
-  end
+module ring_oscillator #(parameter STAGES = 5)(
+    input logic enable,
+    output logic clk_out
+);
+    logic [STAGES-1:0] stage;
+    always_comb begin
+        if (enable) begin
+            stage[0] = ~stage[STAGES-1];
+            for (int i = 1; i < STAGES; i++)
+                stage[i] = stage[i-1];
+            clk_out = stage[STAGES-1];
+        end else begin
+            clk_out = 0;
+        end
+    end
 endmodule
